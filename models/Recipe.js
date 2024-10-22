7dfade70b83cea59ed6db029587b593016ba901a
asiphile <asiphilemthethethwa@gmail.com>
2024-10-22 09:39:22 +0200

    defining the structure of the recipe collection

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,3 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-gi
+const recipeSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  ingredients: { type: [String], required: true },
+  steps: { type: [String], required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
